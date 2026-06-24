cf1dcc69a99833926145cf4a8017c056d60ea0e7
Atharv <atharvnagaonkar@gmail.com>
2026-06-24 15:06:31 +0530

    Remove target and Cargo.lock from tracking

--- dsa/target/debug/deps/dsa-894ce5d0e4c41d5d.d
+++
@@ -1,5 +0,0 @@
-/home/atharv/Codes/DSA/dsa/target/debug/deps/dsa-894ce5d0e4c41d5d.d: src/main.rs
-
-/home/atharv/Codes/DSA/dsa/target/debug/deps/libdsa-894ce5d0e4c41d5d.rmeta: src/main.rs
-
-src/main.rs:
--- dsa/target/debug/deps/dsa-c41fff9099ccfa03.d
+++
@@ -1,5 +0,0 @@
-/home/atharv/Codes/DSA/dsa/target/debug/deps/dsa-c41fff9099ccfa03.d: src/main.rs
-
-/home/atharv/Codes/DSA/dsa/target/debug/deps/dsa-c41fff9099ccfa03: src/main.rs
-
-src/main.rs:
